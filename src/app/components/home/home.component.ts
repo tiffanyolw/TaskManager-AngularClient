2fc3b3653979d27433c69995bd00215a53420257
twong4 <62853188+twong4@users.noreply.github.com>
2021-07-03 14:29:38 -0400

    Data displaying and directives

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  photo: String = "https://www.pngitem.com/pimgs/m/255-2556127_computer-icons-task-or-action-item-icon-png.png";
+  
   constructor() { }
 
   ngOnInit(): void {
